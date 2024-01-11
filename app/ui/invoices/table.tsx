2c2a261be77bdf115f115ee832e01426835d248d
B0dude <ethan10889@gmail.com>
2024-01-10 20:52:56 -0800

    Improved accessibility - Added form validation in both the server-side code and the client-side code. - Used Zod library to create a form schema to validate user inputs. - If user doesn't input all fields, user will get an error. - Fixed eslintrc.json. Had to remove multiple arguments, simplifying it to just one argument. - Added next lint for checking if using aria labels correctly.

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -31,7 +31,7 @@ export default async function InvoicesTable({
                         className="mr-2 rounded-full"
                         width={28}
                         height={28}
-                        alt={`${invoice.name}'s profile picture`}
+                        alt={`${invoice.name}'s profile picture`} // delete line for ESLint test
                       />
                       <p>{invoice.name}</p>
                     </div>
